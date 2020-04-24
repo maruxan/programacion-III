8d048565d36911fe877ec5f0a3312282e4bc9966
maruxan <garavagliamauricio@gmail.com>
2020-04-24 19:31:57 -0300

    Se agregan clases apuesta u jugador

---
+++ dados-con-apuestas/Apuesta.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dados_con_apuestas
+{
+    class Apuesta
+    {
+        private int numero;
+        private ModoApuesta modoApuesta;
+        private float monto;
+
+        public Apuesta(int numero, ModoApuesta modoApuesta, float monto)
+        {
+            this.numero = numero;
+            this.modoApuesta = modoApuesta;
+            this.monto = monto;
+        }
+
+        // En caso de ganar la apuesta
+        public float Pagar()
+        {
+            float monto = 0;
+
+            if (modoApuesta == ModoApuesta.CONSERVADOR)
+            {
+                monto = this.monto * 2;
+            }
+            else if (modoApuesta == ModoApuesta.ARRIESGADO)
+            {
+                monto = this.monto * 5;
+            }
+            else if (modoApuesta == ModoApuesta.DESESPERADO)
+            {
+                monto = this.monto * 15;
+            }
+
+            return monto;
+        }
+        
+        // En caso de perder la apuesta
+        public float Cobrar()
+        {
+            float monto = 0;
+
+            if (modoApuesta == ModoApuesta.CONSERVADOR)
+            {
+                monto = this.monto;
+            }
+            else if (modoApuesta == ModoApuesta.ARRIESGADO)
+            {
+                monto = this.monto * 2;
+            }
+            else if (modoApuesta == ModoApuesta.DESESPERADO)
+            {
+                monto = this.monto * 4;
+            }
+
+            return monto;
+        }
+    }
+
+    public enum ModoApuesta
+    {
+        CONSERVADOR, ARRIESGADO, DESESPERADO
+    }
+}
---
+++ dados-con-apuestas/Jugador.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dados_con_apuestas
+{
+    class Jugador
+    {
+        private string nombre;
+        private float saldo;
+        private Apuesta apuesta;
+
+        public Jugador(string nombre, float saldo)
+        {
+            this.nombre = nombre;
+            this.saldo = saldo;
+        }
+
+        public string getNombre()
+        {
+            return this.nombre;
+        }
+
+        public void setSaldo(int saldo)
+        {
+            this.saldo = saldo;
+        }
+
+        // Evalua si el jugador puede pagar la apuesta en caso de perder
+        public bool puedePagar(Apuesta a)
+        {
+            return a.Cobrar() <= saldo;
+        }
+
+        public void setApuesta(int numero, ModoApuesta modoApuesta, float monto)
+        {
+            Apuesta a = new Apuesta(numero, modoApuesta, monto);
+
+            if (puedePagar(a))
+                this.apuesta = a;
+            else
+                Console.WriteLine("La apuesta supera el saldo disponible");
+        }
+    }
+}
